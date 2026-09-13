8226f1eacd31cff2cdef28c06e5a7c477dc83b96
V3DxNT <ved4geeks@gmail.com>
2026-09-13 17:50:14 +0530

    feat : Root CMD

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "ray",
+	Short: "Ray CLI",
+	Long:  `Ray CLI`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Ray CLI v" + cmd.Version)
+	},
+	Version: "1.0.0",
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/V3DxNT/Ray/cmd"
+
+func main() {
+	cmd.Execute()
+}
